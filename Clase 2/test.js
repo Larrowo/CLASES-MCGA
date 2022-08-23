2d01e55eb04415346858c5a23b301823041a1805
Julián Astudillo <julian12astudillo@gmail.com>
2022-08-23 20:46:47 -0300

    Clase 2 folder created

---
+++ Clase 2/test.js
@@ -0,0 +1,11 @@
+function sum(a, b){
+ return a + b;
+}
+console.log(sum(1,2))
+    
+
+
+const test = require('./test2');
+let saludo = test.fran();
+
+console.log(saludo)
---
+++ Clase 2/test2.js
@@ -0,0 +1,6 @@
+function fran(){
+    return "hola Fran";
+}
+
+
+module.exports = {fran}
